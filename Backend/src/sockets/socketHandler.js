a6e25bab7eca4e6ddc8363a9349256ffeb36c0fe
deepak <152182723+DEEPAK-009@users.noreply.github.com>
2026-03-10 19:50:16 +0530

    Implemented active users tracking in rooms

--- Backend/src/sockets/socketHandler.js
+++ Backend/src/sockets/socketHandler.js
@@ -1,19 +1,33 @@
 const roomService = require("../services/roomService");
+const rooms = {};
 
 const socketHandler = (io) => {
   io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
-    socket.on("join-room", ({ roomId, userId }) => {
-      socket.join(roomId);
-      console.log(`${userId} joined room ${roomId}`);
-      // send existing code to the user who joined
-      socket.emit("load-code", {
-        code: room.code
-      });
-      socket.to(roomId).emit("user-joined", {
-        userId,
-        socketId: socket.id
-      });
+
+    socket.on("join-room", async ({ roomId, userId }) => {
+        socket.join(roomId);
+        console.log(`${userId} joined room ${roomId}`);
+        const room = await roomService.joinRoom(roomId, userId);
+        // send existing code to the user
+        socket.emit("load-code", {
+            code: room.code
+        });
+        // track user in memory
+        if (!rooms[roomId]) {
+            rooms[roomId] = [];
+        }
+        rooms[roomId].push({
+            userId,
+            socketId: socket.id
+        });
+        // send updated users list
+        io.to(roomId).emit("users-update", rooms[roomId]);
+        // notify others
+        socket.to(roomId).emit("user-joined", {
+            userId,
+            socketId: socket.id
+        });
     });
 
     socket.on("code-change", async ({ roomId, code }) => {
@@ -28,6 +42,12 @@ const socketHandler = (io) => {
 
     socket.on("disconnect", () => {
       console.log("User disconnected:", socket.id);
+      for (const roomId in rooms) {
+        rooms[roomId] = rooms[roomId].filter(
+        user => user.socketId !== socket.id
+        );
+        io.to(roomId).emit("users-update", rooms[roomId]);
+    }
     });
 
   });
