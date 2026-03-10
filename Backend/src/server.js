2e7eb541ea9af70ed128d3c411e8521dc7b39d26
deepak <152182723+DEEPAK-009@users.noreply.github.com>
2026-03-10 15:53:11 +0530

    initial commit

---
+++ Backend/src/server.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const { Server } = require("socket.io");
+const app = require("./app");
+
+const PORT = process.env.PORT || 5050;
+
+const server = http.createServer(app);
+
+require("dotenv").config();
+const connectDB = require("./config/db");
+const http = require("http");
+const { Server } = require("socket.io");
+const app = require("./app");
+
+connectDB();
+
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
